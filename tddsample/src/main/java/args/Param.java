081d02dbf2363d00c681cad4cece015c8044b610
dxxfire <dxxfire@163.com>
2019-07-30 00:09:59 +0800

    按2位空格缩进进行代码格式化

--- tddsample/src/main/java/args/Param.java
+++ tddsample/src/main/java/args/Param.java
@@ -2,38 +2,38 @@ package args;
 
 public class Param {
 
-    String key;
+  String key;
 
-    String type;
+  String type;
 
-    String value;
+  String value;
 
-    public Param(String key,String type){
-        this.key = key;
-        this.type = type;
-    }
+  public Param(String key,String type){
+    this.key = key;
+    this.type = type;
+  }
 
-    public String getKey(){
-        return key;
-    }
+  public String getKey(){
+    return key;
+  }
 
-    public void setKey(String key){
-        this.key = key;
-    }
+  public void setKey(String key){
+    this.key = key;
+  }
 
-    public String getType(){
-        return type;
-    }
+  public String getType(){
+    return type;
+  }
 
-    public void setType(String type){
-        this.type = type;
-    }
+  public void setType(String type){
+    this.type = type;
+  }
 
-    public String getValue(){
-        return value;
-    }
+  public String getValue(){
+    return value;
+  }
 
-    public void setValue(String value){
-        this.value = value;
-    }
+  public void setValue(String value){
+    this.value = value;
+  }
 }
