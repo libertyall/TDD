1f1fc9287874cc5f6933f0c0d411a54dcf73b018
dxxfire <dxxfire@163.com>
2019-07-29 23:29:40 +0800

    创建一个参数对象

---
+++ tddsample/src/main/java/args/Param.java
@@ -0,0 +1,31 @@
+package args;
+
+public class Param {
+    String key;
+    String type;
+    String value;
+
+    public String getKey(){
+        return key;
+    }
+
+    public void setKey(String key){
+        this.key = key;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+    public void setValue(String value){
+        this.value = value;
+    }
+}
