23dc18423745f79f8192724ab919d2952022a8af
dxxfire <dxxfire@163.com>
2019-07-29 23:04:45 +0800

    创建满足最少测试的Args类逻辑

---
+++ tddsample/src/main/java/args/Args.java
@@ -0,0 +1,16 @@
+package args;
+
+public class Args {
+
+    private final String paramTypes;
+    private final String params;
+
+    public Args(String paramTypes,String params){
+        this.paramTypes = paramTypes;
+        this.params = params;
+    }
+
+    public Object getValue(String l){
+        return true;
+    }
+}
