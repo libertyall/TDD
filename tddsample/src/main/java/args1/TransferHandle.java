155a901185cc986d259479cea70ab13eb4383d34
dxxfire <dxxfire@163.com>
2019-07-30 15:42:02 +0800

    第二轮，最快方式实现根据参数类型转换值的工具方法，用于通过测试。

---
+++ tddsample/src/main/java/args1/TransferHandle.java
@@ -0,0 +1,17 @@
+package args1;
+
+public class TransferHandle {
+
+  private final String type;
+
+  private final String value;
+
+  public TransferHandle(String type,String value){
+    this.type = type;
+    this.value = value;
+  }
+
+  public Object getValue(){
+    return false;
+  }
+}
