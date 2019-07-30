e0037be93fc96f7c981a7a92ddb254ad4bb2ebdd
dxxfire <dxxfire@163.com>
2019-07-30 11:48:45 +0800

    第二轮，编写测试方法，对输入的参数类型字符串处理的工具方法进行测试。

--- tddsample/src/test/java/args/Args1Test.java
+++ tddsample/src/test/java/args/Args1Test.java
@@ -1,10 +1,13 @@
 package args;
 
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import args1.Args1;
+import args1.InStringHandle;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,5 +22,16 @@ public class Args1Test {
   public void test_2(){
     Args1 args1 = new Args1("l:bool,d:int,f:string,s:string","-l true -d -9 -f /usr/local -s");
     assertEquals(args1.getValue("l"),Boolean.TRUE);
+  }
+
+  @Test
+  public void test_2_tools(){
+    InStringHandle handle = new InStringHandle();
+    Map<String, Param> maps = handle.inTypesToParams("l:bool,d:int,f:string,s:string");
+    assertEquals(maps.size(),4);
+    assertEquals(maps.get("l").getKey(),"l");
+    assertEquals(maps.get("l").getType(),"bool");
+    assertEquals(maps.get("d").getType(),"int");
+    assertEquals(maps.get("f").getType(),"string");
   }
 }
