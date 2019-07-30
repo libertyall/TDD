1effb5b32be6c085c39fbfd8640a558039a092b4
dxxfire <dxxfire@163.com>
2019-07-30 16:16:03 +0800

    第二轮，完整实现根据参数类型转换值的工具方法，并通过测试。

--- tddsample/src/main/java/args1/TransferHandle.java
+++ tddsample/src/main/java/args1/TransferHandle.java
@@ -1,10 +1,19 @@
 package args1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TransferHandle {
 
   private final String type;
 
   private final String value;
+
+  private final Map<String,Transfer> transferMap = new HashMap() {{
+    put("bool",new TransferBooleanImpl());
+    put("int",new TransferIntImpl());
+    put("string",new TransferStringImpl());
+  }};
 
   public TransferHandle(String type,String value){
     this.type = type;
@@ -12,6 +21,7 @@ public class TransferHandle {
   }
 
   public Object getValue(){
-    return false;
+    Transfer transfer =  transferMap.get(type);
+    return transfer.transfer(value);
   }
 }
