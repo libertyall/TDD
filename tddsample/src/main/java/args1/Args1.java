d9d2552c4a8771283ed27bb6307d0b62aa5da969
dxxfire <dxxfire@163.com>
2019-07-30 11:39:33 +0800

    第二轮，编写代码满足测试类，达到绿灯状态

---
+++ tddsample/src/main/java/args1/Args1.java
@@ -0,0 +1,16 @@
+package args1;
+
+public class Args1 {
+
+  private final String types;
+  private final String params;
+
+  public Args1(String types,String params){
+    this.types = types;
+    this.params = params;
+  }
+
+  public Object getValue(String l){
+    return true;
+  }
+}
